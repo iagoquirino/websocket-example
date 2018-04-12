741e06ec7bb66f76477bb876d99af74590fa96ea
Iago Quirino <iquirino@google.com>
2018-04-12 14:10:30 -0300

    minor fixes

--- src/main/kotlin/com/websocket/example/Message.kt
+++ src/main/kotlin/com/websocket/example/Message.kt
@@ -1,11 +1,10 @@
 package com.websocket.example;
 
 import java.util.Date;
-import java.time.Instant
 
 data class Message (
- val subject:String = "",
- val content:String = "",
- var sent:Date = Date.from(Instant.now()) ,
- var received:Date = Date.from(Instant.now())
+ val subject:String,
+ val content:String,
+ var sent:Date,
+ var received:Date
 )
